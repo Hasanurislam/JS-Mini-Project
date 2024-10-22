c36b7a70e5884af3fc0e39ef4754c4c4ba9c53ca
hasanurislam <hasanur7811@gmail.com>
2024-10-22 23:03:05 +0530

    quiz app

---
+++ Quiz.js
@@ -0,0 +1,15 @@
+const Question=[
+    {question:"which is the largest animal",
+    answer:[{text:"shark",correct:false},
+        {text:"halephant",correct:false},
+        {text:"weel",correct:true},
+        {text:"lion",correct:false}
+    ]
+    },
+    {
+        question:"who devloped the theory of general relativity",
+        answer:[
+            {text:"Isaac Newton",correct:}
+        ]
+    }
+]
