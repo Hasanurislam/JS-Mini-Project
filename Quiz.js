0c81822894b8dc9446884e33207f143b952534f7
hasanurislam <hasanur7811@gmail.com>
2024-10-23 22:35:19 +0530

    Quiz app

--- Quiz.js
+++ Quiz.js
@@ -9,7 +9,31 @@ const Question=[
     {
         question:"who devloped the theory of general relativity",
         answer:[
-            {text:"Isaac Newton",correct:}
+            {text:"Isaac Newton",correct:false},
+            {text:"Albert Einstei",correct:true},
+            {text:"Galileo Galilei",correct:false},
+            {text:"Marie Curie",correct:false},
+
+            
+        ]
+    },
+    {
+        question:"Which planet is known as the Red Planet?",
+        answer:[
+            {text:"Venus" ,correct:false},
+            {text:"jupitar" ,correct:false},
+            {text:"uranas" ,correct:false},
+            {text:"mars" ,correct:false},
+
+        ]
+    },
+    {
+        question:"What is the largest organ in the human body?",
+        answer:[
+            {text:"skin",correct:true},
+            {text:"hand",correct:false},
+            {text:"liver",correct:false},
+            {text:"eye",correct:false},
         ]
     }
-]
+];
