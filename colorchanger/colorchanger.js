46c2faff1def25ab50dfdbeae15632c8e2d8fb8e
hasanurislam <hasanur7811@gmail.com>
2024-10-26 11:22:11 +0530

    color changer

--- colorchanger/colorchanger.js
+++ colorchanger/colorchanger.js
@@ -1,4 +1,4 @@
-let box1=document.querySelector(".box1");
+/*let box1=document.querySelector(".box1");
 let box2=document.querySelector(".box2");
 let box3=document.querySelector(".box3");
 let box4=document.querySelector(".box4");
@@ -14,4 +14,29 @@ box3.addEventListener("click",function(){
 })
 box4.addEventListener("click",function(){
     document.body.style.backgroundColor="yellow";
+})
+    */
+let box=document.querySelectorAll(".box");
+let body=document.querySelector("body");
+
+box.forEach(function(btn){
+    console.log(btn);
+    btn.addEventListener("click",function(e){
+        console.log(e)
+        console.log(e.target)
+        if(e.target.id=="green"){
+            body.style.backgroundColor=e.target.id;
+        }
+        if(e.target.id=="red"){
+            body.style.backgroundColor=e.target.id;
+        }
+        if(e.target.id=="blue"){
+            body.style.backgroundColor=e.target.id;
+        }
+        if(e.target.id=="yellow"){
+            body.style.backgroundColor=e.target.id;
+        }
+
+    })
+
 })
