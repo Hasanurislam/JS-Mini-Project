0850ae7c81af7cd7e9158ac25807d2a3076e6a38
hasanurislam <hasanur7811@gmail.com>
2024-11-08 23:33:20 +0530

    QR code Generator

---
+++ QRcodeGenerator/logic.js
@@ -0,0 +1,10 @@
+let inp=document.querySelector('.inp');
+
+let select=document.querySelector('#slct');
+let generatebtn=document.querySelector('.btn');
+
+console.log(inp.value);
+
+function generateQRcode(){
+    new  QRCode()
+}
